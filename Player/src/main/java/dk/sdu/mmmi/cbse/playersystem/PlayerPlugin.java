9dc465b03c0ecb079bb944f863a9790cff5b9de5
jonaslissner <jolis21@student.sdu.dk>
2024-05-07 18:53:32 +0200

    Update PlayerPlugin.java

--- Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerPlugin.java
+++ Player/src/main/java/dk/sdu/mmmi/cbse/playersystem/PlayerPlugin.java
@@ -13,24 +13,19 @@ public class PlayerPlugin implements IGamePluginService {
 
     @Override
     public void start(GameData gameData, World world) {
-
-        // Add entities to the world
-        player = createPlayerShip(gameData);
-        world.addEntity(player);
+        world.addEntity(createPlayerShip(gameData));
     }
 
-    private Entity createPlayerShip(GameData gameData) {
-
-        Entity playerShip = new Player();
-        playerShip.setPolygonCoordinates(-5,-5,10,0,-5,5);
-        playerShip.setX(gameData.getDisplayHeight()/2);
-        playerShip.setY(gameData.getDisplayWidth()/2);
-        return playerShip;
+    public Entity createPlayerShip(GameData gameData) {
+        player = new Player();
+        player.setPolygonCoordinates(-5,-5,10,0,-5,5);
+        player.setX(gameData.getDisplayHeight()/2);
+        player.setY(gameData.getDisplayWidth()/2);
+        return player;
     }
 
     @Override
     public void stop(GameData gameData, World world) {
-        // Remove entities
         world.removeEntity(player);
     }
 
